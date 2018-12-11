a5c3d6831b21f3d43d041945baf923de41e2347b
Matt Newman <matt.newman@telegraph.co.uk>
2018-12-11 16:45:57 +0000

    adding intial scripts for hub conversion and backup.

---
+++ hub-backup.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+DEFAULT_ENV='http://aem-docker-training.aws-preprod.telegraph.co.uk:4502/'
+LOCALHOST='http://localhost:4502/'
+ROOTPAGE='crx/packmgr/index.jsp'
+PACKAGE_GROUP='my_packages'
+PACKAGE_PATH="/etc/packages/${PACKAGE_GROUP}/"
+CRX_CREATE_PATH="crx/packmgr/service/.json${PACKAGE_PATH}"
+CRX_UPDATE_PATH='crx/packmgr/update.jsp'
+TIMEOUT=2
+
+path=''
+env=${LOCALHOST}
+auth='admin:admin'
+
+# get input/args:
+#   -p = path/to/page (required)
+#   -e = env (optional - default to training)
+#   -u = username:password (optional - default to admin:admin)
+#   -l = use localhost as env
+#   -v = verbose output
+
+while getopts  "p:e:u:vl" OPTION
+do
+    case $OPTION in
+        p) path=$OPTARG;;
+        e) env=$OPTARG;;
+        u) auth=$OPTARG;;
+        v) verbose=1;;
+        *) exit 1 # illegal option
+    esac
+done
+
+# if env does not end with a forward slash add one
+if [[ ! ${env} =~ /$ ]]; then
+    echo "adding / to env"
+    env="${env}/"
+fi
+
+# if path starts with a forward slash, strip it, because one exists at the end of $env
+if [[ ${path} =~ ^/ ]]; then
+    path="${path:1}"
+fi
+
+# check access to env using username & password
+connectStatus=$(curl --write-out %{http_code} --silent --output /dev/null -I --user $auth -L --connect-timeout $TIMEOUT $env$ROOTPAGE)
+if [[ ! "$connectStatus" == "200" ]]; then
+    echo "env=${env}"
+    echo "path=${path}"
+    echo "couldn't connect to package manager using: curl -IL --user $auth $env$ROOTPAGE"
+    exit 1;
+fi
+
+packageNameStripped=${path//content\/telegraph\/}
+packageNameStripped=${packageNameStripped//\/jcr:content/}
+packageName=${packageNameStripped//\//-}
+packageZip=${PACKAGE_PATH}${packageName}.zip
+
+if [[ ${verbose} ]]; then
+    echo packageNameStripped=${packageNameStripped}
+    echo packageName=${packageName}
+    echo packageZip=${packageZip}
+fi
+
+# create package
+createPackage=$(curl --write-out %{http_code} --silent --output /dev/null -i --user $auth --connect-timeout $TIMEOUT -X POST ${env}${CRX_CREATE_PATH}${packageName}\?cmd\=create -d packageName=${packageName} -d groupName=${PACKAGE_GROUP})
+if [[ ! "$createPackage" == "200" ]]; then
+    echo "failed to create package"
+    echo "curl --write-out %{http_code} --silent --output /dev/null -i --user $auth --connect-timeout $TIMEOUT -X POST ${env}${CRX_CREATE_PATH}${packageName}\?cmd\=create -d packageName=${packageName} -d groupName=${PACKAGE_GROUP}"
+    exit 1;
+fi
+
+# add filters
+addFilters=$(curl --write-out %{http_code} --silent --output /dev/null -i --user $auth --connect-timeout $TIMEOUT -X POST ${env}${CRX_UPDATE_PATH} -F path=${packageZip} -F packageName=${packageName} -F groupName=${PACKAGE_GROUP} -F filter="[{\"root\" : \"/${path}\", \"rules\": []}]" -F '_charset_=UTF-8')
+if [[ ! "$addFilters" == "200" ]]; then
+    echo "failed to add filters"
+    echo "curl --write-out %{http_code} --silent --output /dev/null -i --user $auth --connect-timeout $TIMEOUT -X POST ${env}${CRX_UPDATE_PATH} -F path=${packageZip} -F packageName=${packageName} -F groupName=${PACKAGE_GROUP} -F filter=\"[{\"root\" : \"/${path}\", \"rules\": []}]\" -F '_charset_=UTF-8'"
+    exit 1;
+fi
+
+# build package
+buildPackage=$(curl --write-out %{http_code} --silent --output /dev/null -i --user $auth --connect-timeout $TIMEOUT -X POST ${env}${CRX_CREATE_PATH}${packageName}.zip\?cmd\=build)
+if [[ ! "$buildPackage" == "200" ]]; then
+    echo "failed to build package"
+    echo "curl --write-out %{http_code} --silent --output /dev/null -i --user $auth --connect-timeout $TIMEOUT -X POST ${env}${CRX_CREATE_PATH}${packageName}.zip\?cmd\=build"
+    exit 1;
+fi
---
+++ hub-converter.sh
@@ -0,0 +1,145 @@
+#!/usr/bin/env bash
+
+DEFAULT_ENV='http://aem-docker-training.aws-preprod.telegraph.co.uk:4502/'
+LOCALHOST='http://localhost:4502/'
+ROOTPAGE='libs/cq/core/content/welcome.html'
+TIMEOUT=2
+
+HUB_RENDERER='telegraph/core/commons/renderers/hubRenderer'
+HUB_TEMPLATE='/apps/telegraph/core/commons/templates/hubTemplate'
+
+INDEX_RENDERER='telegraph/core/commons/renderers/indexRenderer'
+INDEX_TEMPLATE='/apps/telegraph/core/commons/templates/indexTemplate'
+
+INDEX_CONTENT_NODE_TYPE='nt:unstructured'
+INDEX_CONTENT_RESOURCE_TYPE='foundation/components/parsys'
+
+path=''
+env=${DEFAULT_ENV}
+auth='admin:admin'
+
+# get input/args:
+#   -p = path/to/page (required)
+#   -e = env (optional - default to training)
+#   -u = username:password (optional - default to admin:admin)
+#   -l = use localhost as env
+#   -i = invert process (change index back to hub)
+#   -v = verbose output
+
+
+while getopts  "p:e:u:ilv" OPTION
+do
+    case $OPTION in
+        p) path=$OPTARG;;
+        e) env=$OPTARG;;
+        l) local=1;;
+        u) auth=$OPTARG;;
+        i) invert=1;;
+        v) verbose=1;;
+        *) exit 1 # illegal option
+    esac
+done
+
+# if only 1 arg, assume its the path
+if [[ $# == 1 ]]; then
+    path=$1;
+fi
+
+# if path starts with a forward slash, strip it, because one exists at the end of $env
+if [[ ${path} =~ ^/ ]]; then
+    path="${path:1}"
+fi
+
+# if path ends with a forward slash, strip it
+if [[ ${path} =~ /$ ]]; then
+    path="${path: : -1}"
+fi
+
+# if path now only has a forward slash, then it was empty
+if [[ ${path} == '/' ]]; then
+    echo "path not supplied or invalid"
+    exit 1;
+fi
+
+# prefix /content/telegraph if not there
+if [[ ! ${path} =~ telegraph/ ]]; then
+    path=content/telegraph/${path}
+fi
+
+# append /jcr:content if not there
+if [[ ! ${path} =~ /jcr:content$ ]]; then
+    path="${path}/jcr:content"
+fi
+
+# if env does not end with a forward slash add one
+if [[ ! ${env} =~ /$ ]]; then
+    env="${env}/"
+fi
+
+# if local flag is there, use localhost
+if [[ ${local} ]]; then
+    env=${LOCALHOST}
+fi
+
+# if you want to switch back
+if [[ ${invert} ]]; then
+    tempRenderer=${INDEX_RENDERER}
+    tempTemplate=${INDEX_TEMPLATE}
+    INDEX_RENDERER=${HUB_RENDERER}
+    INDEX_TEMPLATE=${HUB_TEMPLATE}
+    HUB_RENDERER=${tempRenderer}
+    HUB_TEMPLATE=${tempTemplate}
+fi
+
+if [[ ${verbose} ]]; then
+    echo "env=${env}"
+    echo "path=${path}"
+    echo "INDEX_RENDERER=${INDEX_RENDERER}"
+    echo "INDEX_TEMPLATE=${INDEX_TEMPLATE}"
+    echo "HUB_RENDERER=${HUB_RENDERER}"
+    echo "HUB_TEMPLATE=${HUB_TEMPLATE}"
+fi
+
+# check access to env using username & password
+connectStatus=$(curl --write-out %{http_code} --silent --output /dev/null -I --user $auth -L --connect-timeout $TIMEOUT $env$ROOTPAGE)
+if [[ ! "$connectStatus" == "200" ]]; then
+    echo "couldn't connect using: curl -L --user $auth $env"
+    exit 1;
+fi
+
+# check page exists and is a hubTemplate
+pageJson=$(curl --silent --user $auth -L --connect-timeout $TIMEOUT $env$path.1.json)
+# curl -i --user $auth $env$path
+if [[ ! "$pageJson" =~ ${HUB_RENDERER} ]]; then
+    echo "page doesn't seem to be a hub page..."
+    echo "curl --user $auth -L --connect-timeout $TIMEOUT $env$path.1.json"
+    exit 1;
+fi
+
+
+# TODO:
+# store nodes seen - where ... google doc?
+# post the json response to something that ingests it??
+# flag/output new ones
+
+
+# update the nodes in AEM
+updateStatus=$(curl --write-out %{http_code} --silent --output /dev/null -i --data cq:template=$INDEX_TEMPLATE --data sling:resourceType=$INDEX_RENDERER --user $auth -L --connect-timeout $TIMEOUT $env$path)
+if [[ ! "$updateStatus" == "200" ]]; then
+    echo "failed to update!"
+    echo "using: curl -L --user $auth --data cq:template=${INDEX_TEMPLATE} --data sling:resourceType=${INDEX_RENDERER} --connect-timeout $TIMEOUT $env$path"
+    exit 1;
+fi
+
+# if inverting, then content is already there, don't need to create a new node
+if [[ ${invert} ]]; then
+    exit 0;
+fi
+
+# add content parsys node
+addContentNode=$(curl --write-out %{http_code} --silent --output /dev/null -i --data jcr:primaryType=$INDEX_CONTENT_NODE_TYPE --data sling:resourceType=$INDEX_CONTENT_RESOURCE_TYPE --user $auth -L --connect-timeout $TIMEOUT $env$path/par)
+if [[ ! "$updateStatus" == "200" ]]; then
+    echo "failed to add content node!"
+    echo "using: curl -L --user $auth --data jcr:primaryType=${INDEX_CONTENT_NODE_TYPE} --data sling:resourceType=${INDEX_CONTENT_RESOURCE_TYPE} --connect-timeout $TIMEOUT $env$path/par"
+    exit 1;
+fi
