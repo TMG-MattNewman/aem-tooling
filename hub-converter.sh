3318528b6650dcc79bb04e048fc1c5243a96c044
Matt Newman <matt.newman@telegraph.co.uk>
2018-12-17 19:01:13 +0000

    re-use add-content-node script in hub-convertor.sh

--- hub-converter.sh
+++ hub-converter.sh
@@ -118,8 +118,4 @@ if [[ ${invert} ]]; then
 fi
 
 # add content parsys node
-addContentNode=$(curl --write-out %{http_code} --silent --output /dev/null -i --data jcr:primaryType=$INDEX_CONTENT_NODE_TYPE --data sling:resourceType=$INDEX_CONTENT_RESOURCE_TYPE --user $auth -L --connect-timeout $TIMEOUT $env$path/par)
-if [[ ! "$updateStatus" == "200" ]]; then
-    echo "failed to add content node! using: curl -L --user $auth --data jcr:primaryType=${INDEX_CONTENT_NODE_TYPE} --data sling:resourceType=${INDEX_CONTENT_RESOURCE_TYPE} --connect-timeout $TIMEOUT $env$path/par"
-    exit 1;
-fi
+./add-content-node.sh -p ${path} -e ${env} -${auth}
